314ef57ab5e251e9ed57bb79f1c1715f12d3495d
cpamukcoglu <cpamukc1@students.towson.edu>
2025-04-30 16:28:19 -0400

    Update LearnBudgeting.jsx

--- src/LearnBudgeting.jsx
+++ src/LearnBudgeting.jsx
@@ -10,7 +10,7 @@ const LearnBudgeting = () => {
 
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/chat', {
+      const res = await fetch('http://localhost:5000/ask', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: question }),
