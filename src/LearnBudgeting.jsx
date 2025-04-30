4fc764c3434e584710217d969783db61cae7cab9
cpamukcoglu <cpamukc1@students.towson.edu>
2025-04-30 14:07:45 -0400

    Create LearnBudgeting page

---
+++ src/LearnBudgeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LearnBudgeting = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>AI Budgeting Tutor</h1>
+      <p>This page will eventually teach users how to budget using AI support.</p>
+    </div>
+  );
+};
+
+export default LearnBudgeting;
